d5d56b8afb7c26dc40aea8a127f2ea74bde24e21
Tim Parsons <jutist@gmail.com>
2023-12-06 21:00:02 -0500

    Stable holiday lights

--- holiday/holiday.ino
+++ holiday/holiday.ino
@@ -14,10 +14,6 @@
 // Configurable parameters
 #define MAX_BRIGHTNESS 250      // Brightness for pixels considered the head of the snake
 #define SATURATION_VALUE 255    // How saturated the colors should be
-#define DELAY 10                // The delay, in milliseconds, between each cycle
-
-// Variables to keep track of what color we're on and what the current brightness is
-int hue = 0;
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
@@ -28,6 +24,7 @@ CRGB leds[NUM_LEDS];
 void rainbow();
 void transPride();
 void christmas(int hue);
+void twinkle(int hue, int saturation, int brightness);
 
 void setup() {
   // start the serial port, so we can log data to the Arduino IDE
@@ -44,55 +41,89 @@ void loop() {
   // Do a downard trans-pride
   transPride();
 
-  // Do a red/green thing
-  for (int i = 0; i < 3; i++) {
-    christmas(HUE_RED);
-    christmas(HUE_GREEN);
-  }
+  // // Do a red/green thing
+  christmas(HUE_RED);
+  christmas(HUE_GREEN);
 
-  // Do a magenta/purple thing
-
+  // Twinkle!
+  twinkle(HUE_PURPLE, 255, 80);
 }
 
 void rainbow() {
-  for (int cycle = 0; cycle < NUM_LEDS; cycle++) {
-    for (int i = (NUM_LEDS-1); i >= 0; i--) {
-      if (i >= 234) {
-        // red
-        hue = HUE_RED + cycle;
-      } else if (i < 234 && i >= 201) {
-        // orange
-        hue = HUE_ORANGE + cycle;
-      } else if (i < 201 && i >= 168) {
-        // yellow
-        hue = HUE_YELLOW + cycle;
-      } else if (i < 168 && i >= 135) {
-        // green
-        hue = HUE_GREEN + cycle;
-      } else if (i < 135 && i >= 102) {
-        // aqua
-        hue = HUE_AQUA + cycle;
-      } else if (i < 102 && i >= 69) {
-        // blue
-        hue = HUE_BLUE + cycle;
-      } else if (i < 69 && i >= 36) {
-        // purple
-        hue = HUE_PURPLE + cycle;
-      } else if (i < 36) {
-        // pink
-        hue = HUE_PINK + cycle;
+  int hue = 0;
+  for (int cycle = NUM_LEDS; cycle >= 0; cycle--) {
+    // Set all LEDs to off first
+    for (int x = 0; x < NUM_LEDS; x++) {
+      leds[x] = CRGB(0, 0, 0);
+    }
+
+    // Paint new LEDs
+    for(int i = 0; i < 50; i++) {
+      int led = i + cycle;
+      if (led < NUM_LEDS) {
+        // 0-5 = red
+        // 6-11 = orange
+        // 12-17 = yellow
+        // 18-23 = green
+        // 24-29 = aqua
+        // 30-35 = blue
+        // 36-41 = purple
+        // 42-47 = pink
+        if (i <= 5) {
+          hue = HUE_RED;
+        } else if (i >= 6 && i <= 11) {
+          hue = HUE_ORANGE;
+        } else if (i >= 12 && i <= 17) {
+          hue = HUE_YELLOW;
+        } else if (i >= 18 && i <= 23) {
+          hue = HUE_GREEN;
+        } else if (i >= 24 && i <= 29) {
+          hue = HUE_AQUA;
+        } else if (i >= 30 && i <= 35) {
+          hue = HUE_BLUE;
+        } else if (i >= 36 && i <= 41) {
+          hue = HUE_PURPLE;
+        } else if (i >= 42 && i <= 47) {
+          hue = HUE_PINK;
+        }
+        leds[led] = CHSV(hue, 255, 80);
       }
-      leds[i] = CHSV(hue, SATURATION_VALUE, MAX_BRIGHTNESS);
     }
     FastLED.show();
-#ifdef DELAY
-    FastLED.delay(DELAY);
-#endif
+    FastLED.delay(15);
   }
 }
 
 void transPride() {
+  for (int cycle = NUM_LEDS; cycle >= 0; cycle--) {
+    // Set all LEDs to off first
+    for (int x = 0; x < NUM_LEDS; x++) {
+      leds[x] = CRGB(0, 0, 0);
+    }
 
+    // Paint new LEDs
+    for(int i = 0; i < 50; i++) {
+      int led = i + cycle;
+      if (led < NUM_LEDS) {
+        // 0-9 = blue
+        // 10-19 = pink
+        // 20-29 = white
+        // 30-39 = pink
+        // 40-49 = blue
+        if (i < 10 || i >= 40) {
+          // Blue
+          leds[led] = CHSV(144, 255, 80);
+        } else if ((i >= 10 && i < 20) || (i >= 30 && i < 40)) {
+          // Pink
+          leds[led] = CHSV(224, 255, 80);
+        } else if (i >= 20 && i < 30) {
+          leds[led] = CHSV(0, 0, 80);
+        }
+      }
+    }
+    FastLED.show();
+    FastLED.delay(20);
+  }
 }
 
 void christmas(int hue) {
@@ -103,15 +134,17 @@ void christmas(int hue) {
     }
 
     // Paint new LEDs
-    for(int i = 0; i < 33; i++) {
+    for(int i = 0; i < 66; i++) {
       int led = i + cycle;
       if (led < NUM_LEDS) {
-        leds[led] = CHSV(hue, SATURATION_VALUE, MAX_BRIGHTNESS);
+        leds[led] = CHSV(hue, 255, 80);
       }
     }
     FastLED.show();
-#ifdef DELAY
-    FastLED.delay(DELAY);
-#endif
+    FastLED.delay(5);
   }
+}
+
+void twinkle(int hue, int saturation, int brightness) {
+  
 }
